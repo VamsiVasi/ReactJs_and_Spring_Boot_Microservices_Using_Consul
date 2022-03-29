ce70b484705f326d4a454f19e8ecf09145cc3a32
VamsiVasi <vasikrish999@gmail.com>
2022-03-29 12:32:59 +0530

    Spring Boot Microservices Using Consul

---
+++ Spring_Boot_Microservices/Consul_Training_Service/src/main/java/com/stark/training_service/ConsulTrainingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.stark.training_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsulTrainingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsulTrainingServiceApplication.class, args);
+	}
+
+}
